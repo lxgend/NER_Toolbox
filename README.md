452677598f5e2915105fcb6785206773118fddd1
Xiang Li <lx3103@gmail.com>
2020-07-15 15:21:02 +0800

    add readme.md

---
+++ README.md
@@ -0,0 +1,19 @@
+# NER_ToolBox
+
+| MODEL            | Task    |    F1    |
+| ---------------- | ------- |--------- |
+| **RoBERTa-wwm**  | CLUENER |   0.60   | 
+
+
+## Todo
+- [x] BERT 
+- [ ] CRF
+- [ ] HMM
+- [ ] BERT-CRF
+- [ ] BERT-MRC
+- [ ] BiLSTM-CRF
+- [ ] ...
+
+## Reference
+### data
+* [CLUENER 细粒度命名实体识别](https://github.com/CLUEbenchmark/CLUENER2020)
--- data_processor/cluener/utils_ner.py
+++ data_processor/cluener/utils_ner.py
@@ -1,7 +1,9 @@
 import csv
 import json
+
 import torch
 from transformers import BertTokenizer
+
 
 class CNerTokenizer(BertTokenizer):
     def __init__(self, vocab_file, do_lower_case=False):
@@ -19,6 +21,7 @@ class CNerTokenizer(BertTokenizer):
             else:
                 _tokens.append('[UNK]')
         return _tokens
+
 
 class DataProcessor(object):
     """Base class for data converters for sequence classification data sets."""
@@ -46,15 +49,15 @@ class DataProcessor(object):
             return lines
 
     @classmethod
-    def _read_text(self,input_file):
+    def _read_text(self, input_file):
         lines = []
-        with open(input_file,'r') as f:
+        with open(input_file, 'r') as f:
             words = []
             labels = []
             for line in f:
                 if line.startswith("-DOCSTART-") or line == "" or line == "\n":
                     if words:
-                        lines.append({"words":words,"labels":labels})
+                        lines.append({"words": words, "labels": labels})
                         words = []
                         labels = []
                 else:
@@ -66,33 +69,34 @@ class DataProcessor(object):
                         # Examples could have no label for mode = "test"
                         labels.append("O")
             if words:
-                lines.append({"words":words,"labels":labels})
-        return lines
-
-    @classmethod
-    def _read_json(self,input_file):
-        lines = []
-        with open(input_file,'r') as f:
-            for line in f:
-                line = json.loads(line.strip())
-                text = line['text']
-                label_entities = line.get('label',None)
-                words = list(text)
-                labels = ['O'] * len(words)
-                if label_entities is not None:
-                    for key,value in label_entities.items():
-                        for sub_name,sub_index in value.items():
-                            for start_index,end_index in sub_index:
-                                assert  ''.join(words[start_index:end_index+1]) == sub_name
-                                if start_index == end_index:
-                                    labels[start_index] = 'S-'+key
-                                else:
-                                    labels[start_index] = 'B-'+key
-                                    labels[start_index+1:end_index+1] = ['I-'+key]*(len(sub_name)-1)
                 lines.append({"words": words, "labels": labels})
         return lines
 
-def get_entity_bios(seq,id2label):
+    @classmethod
+    def _read_json(self, input_file):
+        lines = []
+        with open(input_file, 'r') as f:
+            for line in f:
+                line = json.loads(line.strip())
+                text = line['text']
+                label_entities = line.get('label', None)
+                words = list(text)
+                labels = ['O'] * len(words)
+                if label_entities is not None:
+                    for key, value in label_entities.items():
+                        for sub_name, sub_index in value.items():
+                            for start_index, end_index in sub_index:
+                                assert ''.join(words[start_index:end_index + 1]) == sub_name
+                                if start_index == end_index:
+                                    labels[start_index] = 'S-' + key
+                                else:
+                                    labels[start_index] = 'B-' + key
+                                    labels[start_index + 1:end_index + 1] = ['I-' + key] * (len(sub_name) - 1)
+                lines.append({"words": words, "labels": labels})
+        return lines
+
+
+def get_entity_bios(seq, id2label):
     """Gets entities from sequence.
     note: BIOS
     Args:
@@ -136,7 +140,8 @@ def get_entity_bios(seq,id2label):
             chunk = [-1, -1, -1]
     return chunks
 
-def get_entity_bio(seq,id2label):
+
+def get_entity_bio(seq, id2label):
     """Gets entities from sequence.
     note: BIO
     Args:
@@ -166,29 +171,31 @@ def get_entity_bio(seq,id2label):
         elif tag.startswith('I-') and chunk[1] != -1:
             _type = tag.split('-')[1]
 
-         if _type == chunk[0]:
-                chunk[2] = indx
+        if _type == chunk[0]:
+            chunk[2] = indx
 
-            if indx == len(seq) - 1:
-                chunks.append(chunk)
+        if indx == len(seq) - 1:
+            chunks.append(chunk)
         else:
             if chunk[2] != -1:
                 chunks.append(chunk)
             chunk = [-1, -1, -1]
     return chunks
 
-def get_entities(seq,id2label,markup='bios'):
+
+def get_entities(seq, id2label, markup='bios'):
     '''
     :param seq:
     :param id2label:
     :param markup:
     :return:
     '''
-    assert markup in ['bio','bios']
-    if markup =='bio':
-        return get_entity_bio(seq,id2label)
+    assert markup in ['bio', 'bios']
+    if markup == 'bio':
+        return get_entity_bio(seq, id2label)
     else:
-        return get_entity_bios(seq,id2label)
+        return get_entity_bios(seq, id2label)
+
 
 def bert_extract_item(start_logits, end_logits):
     S = []
